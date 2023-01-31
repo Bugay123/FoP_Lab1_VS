51fe17bbe9cd6d597bcac7e3eecc721ca99eba87
Alex Bugay <bosco.bugai@gmail.com>
2023-02-01 00:13:13 +0200

    Getting to know C#

---
+++ Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿//using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Console.OutputEncoding = Encoding.Unicode;
+        //Console.InputEncoding = Encoding.Unicode;
+        Console.WriteLine("Бугай Олександр, група I21");
+        Console.WriteLine("Введіть своє прізвище:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Вiтаю, " + name);
+    }
+}
